4f89a80e6eead37fcc909c9c65d12f7039c208f2
Markus <kyo@Markuss-MacBook-Air.local>
2026-04-02 10:49:23 +0300

    Ül 1 Header and Meals komponentide lisamine rakendusele

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header.js";
+import Meals from "./components/Meals.js";
+
 const App = () => {
- return (
+  return (
     <>
-     <h1>Food Order App</h1>
+      <Header />
+      <Meals />
     </>
   );
-}
+};
 
 export default App;
